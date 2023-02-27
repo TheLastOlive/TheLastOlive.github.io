1b889983479f501c75dfd17b5e02d3a64c0c9158
TheLastOlive <103273392+TheLastOlive@users.noreply.github.com>
2023-02-26 19:52:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-/// script.js
+// Set up the authorization flow
+const clientId = "86cb0bec8eec441cbe481eab050ceb44";
+const redirectUri = "http://localhost:8000/callback.html";
+const scope = "user-read-private user-read-email";
+const state = generateRandomString(16);
+
+function generateRandomString(length) {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+function getAuthorizationUrl() {
+  const url = "https://accounts.spotify.com/authorize";
+  const query = `client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=${encodeURIComponent(state)}`;
+  return `${url}?${query}`;
+}
+
 // Define a function to handle form submission
 function submitForm(event) {
   event.preventDefault(); // Prevent the form from submitting normally
@@ -7,7 +27,7 @@ function submitForm(event) {
   const musicianName = document.getElementById("musician-name").value;
 
   // Define a function to play a track for 5 seconds and then pause it
-  function playTrack() {
+  function playTrack(accessToken) {
     // Call the Spotify API to get the top tracks for the musician
     fetch(`https://api.spotify.com/v1/search?q=${musicianName}&type=artist&limit=1`, {
       headers: {
@@ -41,19 +61,33 @@ function submitForm(event) {
     .catch(error => console.error(error));
   }
 
-  // Get an access token for the Spotify API and then play the track
-  const clientId = "86cb0bec8eec441cbe481eab050ceb44";
-  const clientSecret = "e83a743f54bd4e9fa4f88efa2bc9bb72";
-  let accessToken = "";
-  fetch(`https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`)
+  // Redirect the user to the authorization page
+  window.location.href = getAuthorizationUrl();
+
+  // After the user logs in and is redirected back to the site with an access token, play the track
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+  const returnedState = urlParams.get("state");
+  if (code && returnedState === state) {
+    fetch(`https://accounts.spotify.com/api/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(redirectUri)}&client_id=${clientId}&client_secret=${clientSecret}`
+    })
     .then(response => response.json())
     .then(data => {
-      accessToken = data.access_token;
-      playTrack();
+      const accessToken = data.access_token;
+      playTrack(accessToken);
     })
     .catch(error => console.error(error));
+  }
 }
 
 // Add an event listener to the form to handle form submission
 const form = document.getElementById("musician-form");
 form.addEventListener("submit", submitForm);
+
+
+
