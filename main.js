c096009b72c3fc8ade32ef3fab24a4b9dbc3e803
TheLastOlive <103273392+TheLastOlive@users.noreply.github.com>
2023-11-10 13:12:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const button = document.getElementById("button");
+const display = document.getElementById("header");
+button.addEventListener("click", displayMessage);
 function displayMessage(){
   
 }
